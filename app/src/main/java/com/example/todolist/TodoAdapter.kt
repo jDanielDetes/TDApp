5d2049a9bf9a30f01ad999ad3f0fa209e31b4f60
jDanielDetes <jdanieldetes@gmail.com>
2020-12-21 01:54:11 -0500

    list made and 50% done functioning

---
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.todolist
+
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_todo.view.*
+
+class TodoAdapter (
+    private val todos: MutableList<Todo>
+) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>(){
+
+    class TodoViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+    return TodoViewHolder(
+        LayoutInflater.from(parent.context).inflate(
+            R.layout.item_todo,
+            parent,
+            false
+
+        )
+    )
+
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+
+    private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked:Boolean){
+        if(isChecked){
+            tvTodoTitle.paintFlags =  tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+        } else {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val currentTodo = todos[position]
+        holder.itemView.apply {
+            tv_TodoTitle.text = currentTodo.title
+            cb_Done.isChecked = currentTodo.isChecked
+        }
+    }
+}
